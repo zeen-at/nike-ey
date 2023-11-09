95bfe3cad4e1621907b07d8e8ab1f341f76093b9
zeen-at <sannizinat@gmail.com>
2023-11-09 11:44:42 +0100

    completed project

---
+++ src/components/Button.jsx
@@ -0,0 +1,21 @@
+
+const Button = ({label, iconUrl, backgroundColor, borderColor, textColor, fullWidth}) => {
+  return (
+      <button
+          className={`flex justify-center items-center gap-2 px-7 py-4 
+          border font-montserrat text-lg leading-none rounded-full ${fullWidth && "w-full"} 
+          ${backgroundColor
+          ? `${backgroundColor} ${borderColor} ${textColor}`
+        : "bg-coral-red text-white border-coral-red "
+        } 
+        `}
+      >
+      {label}
+      {iconUrl &&
+        <img src={iconUrl} alt="arrow right" className="ml-2 rounded-full w-5 h-5" />
+      }
+      </button>
+  )
+}
+
+export default Button
---
+++ src/sections/index.jsx
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./footer";
+import SuperQuality from "./superQuality";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./specialOffer";
+import Services from "./services";
+import CustomerReviews from "./customerReviews";
+import Subscribe from "./subscribe";
+
+export {
+    Hero,
+    Footer,
+    Subscribe,
+    SuperQuality,
+    PopularProducts,
+    SpecialOffer,
+    Services,
+    CustomerReviews
+}
